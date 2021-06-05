41d7740349a030722b0e86dc111977bbefc88c3c
AnastasiaSlova <71496897+AnastasiaSlova@users.noreply.github.com>
2021-06-05 13:54:58 +0500

    Add files via upload

---
+++ Hw1.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class Hw1 {
+    public static void main(String[] args) {
+        int x = 12345;
+        String binaryNumber = Integer.toBinaryString(x);
+        System.out.println(binaryNumber);
+
+
+    }
+}
